5bd2d61a8cfe72f56a1b6017f52ce17e08508551
Bug38 <lguimard38@gmail.com>
2024-08-06 09:03:22 +0200

    fix users-list menu

--- app/lib/components/elements/custom-button.tsx
+++ app/lib/components/elements/custom-button.tsx
@@ -6,7 +6,7 @@ interface CustomButtonProps {
     contentItems?: ReactNode[]
     customClasses?: string
     colorClass?: string
-    callback: CallableFunction
+    callback: (event: React.KeyboardEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement>) => void
     tooltip?: string
     active?: boolean
     show?: boolean
@@ -14,14 +14,14 @@ interface CustomButtonProps {
 }
 export function CustomButton({ contentItems, customClasses, colorClass, callback, tooltip, active }: CustomButtonProps) {
     return (
-        <div title={tooltip} className={`customButton fade-on-mouse-out is-unselectable ${customClasses ? customClasses : ''} ${active == false ? 'fade-text has-background-primary-level' : (colorClass || "has-background-primary-level") + ' is-clickable'}`} {...clickorkey(() => (active != false) ? callback() : {})}>
+        <div title={tooltip} className={`customButton fade-on-mouse-out is-unselectable ${customClasses ? customClasses : ''} ${active == false ? 'fade-text has-background-primary-level' : (colorClass || "has-background-primary-level") + ' is-clickable'}`} {...clickorkey((e) => (active != false) ? callback(e) : {})}>
             {contentItems?.map((item, nb) => <div className="is-flex" key={nb}>{item}</div>)}
         </div>
     )
 }
 export function ButtonMore({ customClasses, colorClass, callback, tooltip, active, show, height }: CustomButtonProps) {
     return (
-        <div title={tooltip} className={`customButtonMore fade-on-mouse-out is-unselectable ${customClasses ? customClasses : ''} ${active == false ? 'fade-text' : (colorClass || "") + ' is-clickable'} ${show == false ? '' : 'is-shown'}`} {...clickorkey(() => (active != false) ? callback() : {})} style={height ? { height: height + 'px', width: height + 'px' } : {}}>
+        <div title={tooltip} className={`customButtonMore fade-on-mouse-out is-unselectable ${customClasses ? customClasses : ''} ${active == false ? 'fade-text' : (colorClass || "") + ' is-clickable'} ${show == false ? '' : 'is-shown'}`} {...clickorkey((e) => (active != false) ? callback(e) : {})} style={height ? { height: height + 'px', width: height + 'px' } : {}}>
             <MoreSVG />
         </div>
     )
--- app/lib/utils/clickorkey.ts
+++ app/lib/utils/clickorkey.ts
@@ -1,8 +1,8 @@
-export function clickorkey(callback: () => void, key?: string) {
+export function clickorkey(callback: (event: React.KeyboardEvent<HTMLDivElement> | React.MouseEvent<HTMLDivElement>) => void, key?: string) {
     const keys = key ? [key] : ["Space", " "]
     return {
-        onClick: () => callback(),
+        onClick: (event: React.MouseEvent<HTMLDivElement>) => callback(event),
         role: "button",
-        onKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => { keys.includes(event.key) && callback() }
+        onKeyDown: (event: React.KeyboardEvent<HTMLDivElement>) => { keys.includes(event.key) && callback(event) }
     }
 }
