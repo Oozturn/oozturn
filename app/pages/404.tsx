1a4257030d080f35c718816a2e8036cd7b306c44
Bug38 <lguimard38@gmail.com>
2025-06-12 13:36:30 +0200

    updated public images

--- app/pages/404.tsx
+++ app/pages/404.tsx
@@ -15,7 +15,7 @@ export async function loader(): Promise<{
 
 export default function NotFound() {
   return (
-    <div className="is-full-height is-flex-col align-center justify-center has-background-primary-level" style={{ backgroundImage: "url('/page_not_found.gif')", backgroundSize: "5%" }}>
+    <div className="is-full-height is-flex-col align-center justify-center has-background-primary-level" style={{ backgroundImage: "url('/page_not_found.webp')", backgroundSize: "5%" }}>
           <div className="is-title big ">404 - PAGE NOT FOUND</div>
           <div style={{ color: "var(--background-secondary-level)" }}>Arrête de fouiller tu trouveras rien</div>
     </div>
