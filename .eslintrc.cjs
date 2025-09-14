b166d1d29920be1139addcd15208c6350be8c78d
Bug38 <lguimard38@gmail.com>
2025-09-14 11:17:51 +0200

    prettier + settings
    prettier in eslint

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -69,7 +69,8 @@ module.exports = {
         "plugin:@typescript-eslint/recommended",
         "plugin:import/recommended",
         "plugin:import/typescript",
-      ],
+        "prettier"
+      ]
     },
 
     // Node
